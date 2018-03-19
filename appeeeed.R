c98fd0066fd3c03e7e1c62269235f2ac4f4cd69a
fsoubes <franck.soubes@hotmail.fr>
2018-03-19 15:55:50 +0100

    rm

--- appeeeed.R
+++ appeeeed.R
@@ -1,10 +1,10 @@
 ##################################
 ##################################
 ##                              ##
-## Shiny application            ##
+##  Shiny application            ##
 ##################################
 ##                              ##
-## Author: Franck Soubès        ##
+##  Author: Franck Soubès        ##
 ##################################
 ##################################
 
@@ -156,92 +156,6 @@ ui <- fluidPage(theme = shinytheme("united"),
                     
                   )
                   
-                  # ),
-                  # navbarPage('Testing App',
-                  #
-                  #            headerPanel("Import data files"),
-                  #            #sidebarLayout(
-                  #
-                  #            sidebarPanel(width = 3,
-                  #
-                  #                         tabsetPanel(id = "tabset",
-                  #                                     tabPanel("Heatmap",
-                  #
-                  #                                              fileInput("file1","Choose your csv file",accept = c(
-                  #                                                "text/csv",
-                  #                                                "text/comma-separated-values,text/plain",
-                  #                                                ".csv")
-                  #                                                , multiple = T), style=" font-size:100%; font-family:Arial;
-                  #                                              border-color: #2e6da4; background-color: #337ab7, width: 28px; ",
-                  #                                              #tags$style("#myNumericInput {font-size:10px;height:10px;}"),
-                  #
-                  #                                              downloadButton("save", "save your plot"),
-                  #                                              hr(),
-                  #
-                  #                                              numericInput('clusters', 'Cluster count', 3,
-                  #                                                           min = 1, max = 15),
-                  #                                              hr(),
-                  #
-                  #                                              sliderInput("pval", "P-value:",
-                  #                                                          min = 0.01, max = 0.05,
-                  #                                                          value = 0.05, step = 0.01),
-                  #                                              hr(),
-                  #
-                  #                                              selectInput("dist", "Choix de la distance",
-                  #                                                          choices = c("cor","euclidian")),
-                  #                                              hr(),
-                  #
-                  #                                              actionButton("first", "Print Heatmap",style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
-                  #
-                  #                                              wellPanel(uiOutput("individusel")
-                  #
-                  #
-                  #                                              )),
-                  #                                     tabPanel("PCA",
-                  #
-                  #                                              numericInput("normCount", "Count", 100),
-                  #                                              numericInput("normMean", "Mean", 0),
-                  #                                              numericInput("normSd", "Std Dev", 1)
-                  #
-                  #                                     ),
-                  #
-                  #
-                  #                                     tabPanel("Display data",
-                  #
-                  #                                              DT::dataTableOutput("mytable3"),
-                  #
-                  #                                              fileInput("file2","Choose your csv file",accept = c(
-                  #                                                "text/csv",
-                  #                                                "text/comma-separated-values,text/plain",
-                  #                                                ".csv")
-                  #                                                , multiple = T), style=" font-size:100%; font-family:Arial;
-                  #   border-color: #2e6da4; background-color: #337ab7, width: 28px; "
-                  #
-                  #                                              , actionButton("second", "Print Data",style="color: #fff; background-color: #337ab7; border-color: #2e6da4"))
-                  #
-                  #                         )),
-                  #
-                  #            # conditionalPanel(
-                  #            # 'input.dataset === "atom"',
-                  #            # helpText("Click the column header to sort a column."))),
-                  #
-                  #            # Main panel for displaying outputs ----
-                  #            mainPanel(
-                  #
-                  #              tags$style(type="text/css",
-                  #                         ".shiny-output-error { visibility: hidden; }",
-                  #                         ".shiny-output-error:before { visibility: hidden; }"), ### no more error messages
-                  #
-                  #              bsAlert("alert"),
-                  #              #tableOutput(ouputId= "mytable3"),
-                  #              #tabPanel("seq", DT::dataTableOutput("mytable3")),
-                  #              # Output: Plot
-                  #
-                  #              plotOutput(outputId = "distPlot")
-                  #
-                  #            )
-                  #
-                  # )
                 ))
 
 server <- function(input, output, session) {
@@ -380,7 +294,6 @@ server <- function(input, output, session) {
       choix_individus()
     })
     
-    # observeEvent(input$first, {
     
     formated <- reactive({
       df <- csvf()
