bf47174bb697d9dc364312aa026205484485c111
fsoubes <franck.soubes@hotmail.fr>
2018-06-29 09:21:12 +0200

    without dbi

--- LoadPackages.R
+++ LoadPackages.R
@@ -2,14 +2,14 @@
 #sudo apt-get install libv8-dev
 
 
-list.of.packages <- c("shiny","shinythemes","shinyjs","AnnotationDbi","ggplot2","shinyBS","markdown"
+list.of.packages <- c("shiny","shinythemes","shinyjs","ggplot2","shinyBS","markdown"
                       ,"BH","data.table","DT","readr","colourpicker","shinydashboard","shinytoastr",
                       "tools","devEMF","R.devices","FactoMineR","factoextra","gplots","V8",
                       "RColorBrewer","foreach","doParallel","VennDiagram","gridExtra","plotly","dplyr","reticulate","Hmisc")
 
 #"goseq","GO.db","rbenchmark","heatmaply"
 
-
+#AnnotationDbi
 
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
