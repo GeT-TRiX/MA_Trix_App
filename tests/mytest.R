26b6f8026e78ed7633e1d68227105a559ff4009a
fsoubes <franck.soubes@hotmail.fr>
2018-06-30 07:57:11 +0200

    Update mytest.R

--- tests/mytest.R
+++ tests/mytest.R
@@ -3,18 +3,9 @@ app$snapshotInit("mytest")
 
 app$setInputs(sidebarCollapsed = FALSE)
 app$uploadFile(file = c("All_topTableAll.csv", "TOXA_HEGU_MA0191_AllChip_pData.csv", "TOXA_HEGU_MA0191_AllChip_WorkingSet.csv"))
-# Input 'new_group_rows_current' was set, but doesn't have an input binding.
-# Input 'new_group_rows_all' was set, but doesn't have an input binding.
-# Input 'data_summary_rows_current' was set, but doesn't have an input binding.
-# Input 'data_summary_rows_all' was set, but doesn't have an input binding.
-# Input 'new_test_rows_current' was set, but doesn't have an input binding.
-# Input 'new_test_rows_all' was set, but doesn't have an input binding.
 app$setInputs(method = "None")
-# Input 'data_summary_rows_current' was set, but doesn't have an input binding.
-# Input 'data_summary_rows_all' was set, but doesn't have an input binding.
 app$setInputs(pval1 = 0.01)
-# Input 'data_summary_rows_current' was set, but doesn't have an input binding.#
-# Input 'data_summary_rows_all' was set, but doesn't have an input binding.
+Sys.sleep(2)
 app$snapshot()
 app$setInputs(side = "PCA")
 app$setInputs(indivpca = c("LKO_CTRL", "LKO_MCD ", "LWT_CTRL"))
@@ -36,8 +27,5 @@ app$setInputs(allTests = "click")
 app$setInputs(fc = 2)
 app$setInputs(pval = 0.04)
 app$setInputs(heatm = "click")
-# Input 'clustering_rows_current' was set, but doesn't have an input binding.
-# Input 'clustering_rows_all' was set, but doesn't have an input binding.
-# Input 'totalgenbyc_rows_current' was set, but doesn't have an input binding.
-# Input 'totalgenbyc_rows_all' was set, but doesn't have an input binding.
+Sys.sleep(2)
 app$snapshot()
