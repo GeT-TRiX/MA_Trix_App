0f761e27f8847396b78ece1c26390d19a6831076
fsoubes <franck.soubes@hotmail.fr>
2018-08-04 20:15:43 +0200

    test parallel

--- server/highchartshiny.R
+++ server/highchartshiny.R
@@ -7,18 +7,36 @@ axisParameters <- list(
 )
 
 filteredata<- reactive({
+  
+  #d <- NULL
   req(myresdavitab())
-  reumdiff = lapply(1:length(myresdavitab()),function(x)return(sapply(length(myresdavitab()[[x]]$Count), function(y){
-    return(as.numeric(as.character(myresdavitab()[[x]]$Count))/as.numeric(as.character(myresdavitab()[[x]]$List.Total))*100)})) %>%  
-      mutate(myresdavitab()[[x]],percent = .)) %>% rbind.fill() 
+  # paraltest <- myresdavitab()
+  # cl <- makeCluster(getOption("cl.cores", 4))
+  # clusterExport(cl,c("paraltest"),envir=environment())
+  # clusterEvalQ(cl, library(dplyr))
+   reumdiff = lapply(1:length(myresdavitab()),function(x)return(sapply(length(myresdavitab()[[x]]$Count), function(y){
+     return(as.numeric(as.character(myresdavitab()[[x]]$Count))/as.numeric(as.character(myresdavitab()[[x]]$List.Total))*100)})) %>%  
+       mutate(myresdavitab()[[x]],percent = .)) %>% rbind.fill() 
+  # d = parLapply(cl, 1:length(paraltest),function(x)return(sapply(length(paraltest[[x]]$Count), function(y){
+  #   return(as.numeric(as.character(paraltest$Count))/as.numeric(as.character(paraltest[[x]]$List.Total))*100)})) %>%  
+  #     mutate(paraltest[[x]],percent = .)) %>% rbind.fill() 
+  # stopCluster(cl)
+  #})
+  #d
   return(reumdiff)
 })
 
 plotDataenrichment <- reactive({
-  
+  req(filteredata())
   param <- list(search= "Fold.Enrichment", n_points=length(filteredata()$Fold.Enrichment), x_start=min(as.numeric(filteredata()$Fold.Enrichment)))
   filtered <- filteredata()
   return(DftoHighjson(filtered,param))
+  
+})
+
+observe({
+  req(filteredata())
+  print(filteredata())
   
 })
 
