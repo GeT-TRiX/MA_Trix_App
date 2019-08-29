cdd7d9b8497d4c7d3aba018905307a6e710b95b6
ylippi <y.lippi@gmail.com>
2019-08-29 15:24:42 +0200

    save volcano

--- module/savemodules.R
+++ module/savemodules.R
@@ -111,9 +111,9 @@ content <- function(file) {
 
     cairo_ps(filename=file, width=10, height=10,pointsize = 12)
 
-  else if(volcform  &&  input$format == "eps")
+##  else if(volcform  &&  input$format == "eps")
 
-    ggsave(file,device=cairo_ps, fallback_resolution = 600)
+##    ggsave(file,device=cairo_ps, fallback_resolution = 600)
 
   else
       eps(file,
